4d33cba22b5234315db23a9af1e6366a3553f550
VitorPaludetto <vitorhugopaludetto@outlook.com>
2024-05-03 16:50:30 -0300

    respond to PING

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,18 @@
-import * as net from "node:net";
+import * as net from 'node:net';
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+console.log('Logs from your program will appear here!');
 
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
+
+  connection.on('data', () => {
+    connection.write('+PONG\r\n');
+  });
+  connection.on('end', () => {
+    console.log('client disconnected');
+  });
 });
 //
-server.listen(6379, "127.0.0.1");
-
+server.listen(6379, '127.0.0.1');
